042d45f047a426c85496d3e20183a8d4e1ab51a9
eugene <eugle2000@naver.com>
2024-04-11 17:33:19 +0900

    edit_top

--- uart/uart_top.v
+++ uart/uart_top.v
@@ -1,1 +1,38 @@
-module uart_top()
+module uart_top(
+    input       i_clk,
+    //input       i_start,
+    input       i_reset,
+    input [7:0] sw,
+    input       i_rxd,
+    input       i_bo,
+    output      o_txd,
+    output [7:0]  o_data
+);
+
+wire            w_clk_tx;
+
+uart_tx         tx(
+    .i_clk(i_clk),
+    //.i_start(i_start),
+    .i_reset(i_reset),
+    .i_clk_tx(w_clk_tx),
+    .sw(sw),
+    .i_bo(i_bo),
+    .o_txd(o_txd)
+);
+
+uart_rx         rx(
+    .i_clk(i_clk),
+    .i_clk_rx(w_clk_tx),
+    .i_reset(i_reset),
+    .i_rxd(i_rxd),
+    .o_data(o_data)
+);
+
+tx_clk_div      tx_clk(
+    .i_clk(i_clk),
+    .i_reset(i_reset),
+    .o_clk_out(w_clk_tx)
+);
+
+endmodule
